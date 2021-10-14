6d567f1bf3cc39eb21a43ca05362962a281d2864
Sebastian Gamba <sebastiangamba.music@gmail.com>
2021-10-14 11:30:13 -0500

    validaciones formularios entidad usuario

--- SistemaBibliotecario.App.Dominio/Entidades/Usuario.cs
+++ SistemaBibliotecario.App.Dominio/Entidades/Usuario.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
 using System;
 
 namespace SistemaBibliotecario.App.Dominio.Entidades
@@ -5,12 +7,30 @@ namespace SistemaBibliotecario.App.Dominio.Entidades
     public class Usuario
     {
       public int id{get;set;}
+
+      [Requerido, StringLengthAttribute(50), Display(Name = "DNI:")]
       public string usu_identificacion{get;set;}
+      
+      [Requerido, StringLengthAttribute(50), Display(Name = "Nombres:")]
       public string usu_nombre{get;set;}
+
+      [Requerido, StringLengthAttribute(50), Display(Name = "Apellidos:")]
       public string usu_apellido{get;set;}
+
+      [Requerido, StringLengthAttribute(100), Display(Name = "Dirección:")]
       public string usu_direccion{get;set;}
+
+      [Requerido, StringLengthAttribute(50), Display(Name = "Correo electrónico:")]
       public string usu_email{get;set;}
+      
+      [Requerido, StringLengthAttribute(50), Display(Name = "Telefono:")]
       public string usu_telefono{get;set;}
-  
+    }
+    public class Requerido : RequiredAttribute
+    {
+      public Requerido()
+      {
+          this.ErrorMessage = "Campo requerido, por favor ingrese datos.";
+      }
     }
 }
